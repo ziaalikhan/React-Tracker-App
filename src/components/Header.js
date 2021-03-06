669d924f37f2179c5759ea14e67c8d162f7e1c41
zia khan <ziaalikhan052@gmail.com>
2021-03-06 21:37:50 +0500

    Initail commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css'
+
+const Header = () => {
+    return (
+        <div className='_head'>
+            <h2>Expence Tracker</h2>
+            
+        </div>
+    )
+}
+
+export default Header;
+
